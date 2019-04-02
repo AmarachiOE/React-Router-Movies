515c1a265f6aa3ea40cba7ec8159312c22fd952c
AmarachiOE <ao.ejiawoko@gmail.com>
2019-04-01 19:19:57 -0700

    STRETCH: Added Save Movie functionality

--- client/src/App.js
+++ client/src/App.js
@@ -23,10 +23,16 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <Route exact path="/" render={props => <MovieList {...props} />} />
+        <Route exact path="/" render={props => 
+          <MovieList {...props} />}
+        />
         <Route
           path="/movies/:movieId"
-          render={props => <Movie {...props} movie={this.state.savedList} />}
+          render={props => 
+          <Movie 
+            {...props} 
+            addToSavedList={this.addToSavedList} 
+          />}
         />
       </div>
     );
